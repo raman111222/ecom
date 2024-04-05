3fa5d7347badd73c822148f7d3a5dd55fe6d09fc
raman111222 <singhraman313004@gmail.com>
2024-04-05 23:01:49 +0530

    ui

--- Components/navbar.tsx
+++ Components/navbar.tsx
@@ -2,41 +2,46 @@ import Button from "./button";
 import Image from "next/image";
 import { Roboto } from "next/font/google";
 import Link from "next/link";
+
 const inter = Roboto({
   subsets: ["latin"],
   display: "swap",
   weight: "400",
 });
+
 export default function Navbar() {
   return (
-    <div
-      className={
-        " p-10 flex font-semibold text-white h-[60px] w-screen border-b-1 border-white justify-between items-center"
-      }
+    <nav
+      className=" p-10 flex font-semibold text-white h-[60px] w-screen border-b-1 border-white justify-between items-center"
+      role="navigation"
+      aria-label="Main"
     >
-      <div className="flex gap-10">
-        <div>
+      <div className="flex gap-10" role="menubar">
+        <div role="none">
           <Link href={"/"}>Home</Link>
         </div>
-        <div>
-          {" "}
+        <div role="none">
           <Link href={"/Shop"}>Shop</Link>
         </div>
-        <div>
+        <div role="none">
           <Link href={"/Contact"}>Contact us</Link>
         </div>
       </div>
       <div className={inter.className + " text-3xl"}>
         <Link href={"/"}>
-          <Image alt="logo" src={"/ll.png"} width={200} height={200} />
+          <Image alt="Company logo" src={"/ll.png"} width={200} height={200} />
         </Link>
       </div>
-      <div className="flex gap-7">
-        <div className=" flex items-center justify-center">Login</div>
-        <div>
+      <div className="flex gap-7 items-center" role="menubar">
+        <div role="none">
+          <Link href={"/Login"}>
+            <div className="flex items-center justify-center">Login</div>
+          </Link>
+        </div>
+        <div role="none" className="flex items-center justify-center">
           <Button text="Sign-up" />
         </div>
       </div>
-    </div>
+    </nav>
   );
 }
